e9ca83de7403f75ee52cca03a8a08c2f65637c07
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-02-07 12:15:47 -0400

    (update) Atulizando setup de configuração do projeto.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo chmod +x menu.sh
+chmod +x .src/*.sh
 sudo apt install -y curl jq gpg
-
+clear
 mkdir ~/config-ubuntu-based/logs
+clear
+echo "Rode './menu.sh' para iniciar o processo de configuração do sistema!"
